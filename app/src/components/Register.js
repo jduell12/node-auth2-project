b90756b3431e5017ec4803b337e88a51039702fe
Jessica <duellje@oregonstate.edu>
2020-08-18 18:21:07 -0400

    added form for register component

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const initialFormValues = {
+    username: "",
+    password: "",
+    department: "",
+  };
+
+  const [formValues, setValues] = useState(initialFormValues);
+
+  const changeHandler = (eve) => {
+    const { name, value } = eve.target;
+
+    setValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const submitForm = (eve) => {
+    eve.preventDefault();
+  };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <form onSubmit={submitForm}>
+      <label htmlFor="username">
+        Username: &nbsp;
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formValues.username}
+          onChange={changeHandler}
+        />
+      </label>
+      <label htmlFor="password">
+        Password: &nbsp;
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formValues.password}
+          onChange={changeHandler}
+        />
+      </label>
+      <label htmlFor="department">
+        Department: &nbsp;
+        <input
+          type="text"
+          id="department"
+          name="department"
+          value={formValues.department}
+          onChange={changeHandler}
+        />
+      </label>
+      <button>Register</button>
+    </form>
   );
 };
 
