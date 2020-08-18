a1e728f039ce2af8db56fe02e14c9e1d72cc4c11
Jessica <duellje@oregonstate.edu>
2020-08-18 16:07:20 -0400

    set up login page

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 const Login = () => {
+  const changeHandler = () => {};
+
+  const submitForm = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <h2>Login</h2>
+      <form onSubmit={submitForm}>
+        <label htmlFor="username">
+          Username: &nbsp;
+          <input
+            id="username"
+            type="text"
+            name="username"
+            onChange={changeHandler}
+          />
+        </label>
+        <label>
+          Password: &nbsp;
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={changeHandler}
+          />
+        </label>
+        <button>Login</button>
+      </form>
     </div>
   );
 };
