3607640baf097fcd96e4a77b309d8e65f819a269
Jessica <duellje@oregonstate.edu>
2020-08-18 14:40:33 -0400

    added middleware to check fot jwt

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const constants = require("../config/constants");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, constants.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.jwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+};
