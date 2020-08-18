f23cd0b2084b9e56075fca901a10ba2b63aadaf5
Jessica <duellje@oregonstate.edu>
2020-08-18 18:26:19 -0400

    changed

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const userRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
@@ -6,6 +7,7 @@ const authRouter = require("../auth/auth-router");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/users", userRouter);
 server.use("/api", authRouter);
 
