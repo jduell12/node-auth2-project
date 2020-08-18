d9c4feac0b6475f9cacbf7dc8a191a9cb1a7dde5
Jessica <duellje@oregonstate.edu>
2020-08-18 14:27:14 -0400

    added a user router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const userRouter = require("../users/users-router");
+
 const server = express();
+
 server.use(express.json());
+server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "working" });
